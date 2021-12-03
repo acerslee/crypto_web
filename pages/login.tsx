13e566e012995adaf6ab8832c95d996724930167
Alex Lee <alexlee@Alexs-MBP.fios-router.home>
2021-12-03 18:51:26 -0500

    Initialized project

---
+++ pages/login.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Login: NextPage = () => {
+    return(
+        <section>
+            hello
+        </section>
+    )
+}
+
+export default Login
---
+++ pages/signup.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Signup: NextPage = () => {
+    return(
+        <section>
+        hello
+        </section>
+    )
+}
+
+export default Signup
